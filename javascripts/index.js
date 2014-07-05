130684502f66cb68309b1e4c13d439c733127bff
Cedric Stoquer <cstoquer@wizcorp.jp>
2014-07-05 14:42:55 +0900

    separate sound module

--- javascripts/index.js
+++ javascripts/index.js
@@ -27,65 +27,6 @@ var animationArray = [];
 //██▀░▀█▀░▀██▄▀▀▄░▀█▄▀▀▀▄░▀██▀▀░▀▀██▄▀▀▀▀▄█
 //█████████████████████████████████████████
 
-var audioCtx = new webkitAudioContext();
-
-/**▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
- * @function module:loading.loadSound
- * @desc  load an sound file
- *
- * @param {String}   path - file path
- * @param {Function} cb   - asynchronous callback function
- */
-function loadSound(path, cb) {
-	var snd = new Audio();
-	snd.preload = true;
-	snd.loop = false;
-	
-	function onSoundLoad() {
-		cb(null, snd);
-		snd.removeEventListener('canplaythrough', onSoundLoad);
-		snd.removeEventListener('error', onSoundError);
-	}
-
-	function onSoundError() {
-		cb('snd:load');
-		snd.removeEventListener('canplaythrough', onSoundLoad);
-		snd.removeEventListener('error', onSoundError);
-	}
-
-	snd.addEventListener('canplaythrough', onSoundLoad);
-	snd.addEventListener('error', onSoundError);
-	snd.src = path;
-	snd.load();
-}
-
-function playSoundWebkit(sound, vol, pan) { 
-	if (!sound) return;
-
-	var source = audioCtx.createBufferSource();
-	var volume = audioCtx.createGainNode();
-	var panner = audioCtx.createPanner();
-
-	source.connect(panner);
-	panner.connect(volume);
-	volume.connect(audioCtx.destination);
-
-	panner.coneOuterGain  = 0.5;
-	panner.coneOuterAngle = 180;
-	panner.coneInnerAngle = 0;
-
-	panner.setPosition(pan, 0, -0.5);
-	volume.gain.value = vol;
-	source.buffer = sound;
-	
-	source.noteOn(0);
-}
-
-function playSoundAudio(sound, vol) {
-	if (!sound) return;
-	sound.volume = vol;
-	sound.play();
-}
 
 var playSound = playSoundAudio;
 
